85abf0cecd3224f8eac7127b419c49a30b003ea2
filippova_t <tanya-fef@yandex.ru>
2022-09-11 14:09:25 +0800

    домашка 2.3 выполнена

---
+++ src/main/java/pro/sky/java/course2/calculator/Service.java
@@ -0,0 +1,9 @@
+package pro.sky.java.course2.calculator;
+
+public interface Service {
+    String greeting ();
+    public  String plus (Integer num1, Integer  num2);
+    public  String minus (Integer num1, Integer  num2);
+    public  String multiply (Integer num1, Integer  num2);
+    public  String divide (Integer num1, Integer  num2);
+}
