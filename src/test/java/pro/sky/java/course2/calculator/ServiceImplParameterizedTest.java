8a9538c2702f548c2ddc68569c2406345842a85e
filippova_t <tanya-fef@yandex.ru>
2022-11-07 15:36:03 +0800

    домашка 2.12 выполнена и исправлены параметризованные тесты

--- src/test/java/pro/sky/java/course2/calculator/ServiceImplParameterizedTest.java
+++ src/test/java/pro/sky/java/course2/calculator/ServiceImplParameterizedTest.java
@@ -14,33 +14,31 @@ public class ServiceImplParameterizedTest {
 
     @ParameterizedTest
     @MethodSource("paramsForTestAddiction")
-    public void shouldDoCorrectAddiction (Integer num1, Integer num2) {
+    public void shouldDoCorrectAddiction (Integer num1, Integer num2, Integer expected) {
         String actual = serviceImpl.plus(num1, num2);
-        Integer result = num1 + num2;
-        Assertions.assertEquals (actual, num1 + " + " + num2 + " = " + result);
+
+        Assertions.assertEquals (actual, num1 + " + " + num2 + " = " + expected);
 
     }
 
     @ParameterizedTest
     @MethodSource("paramsForTestSubtraction")
-    public void shouldDoCorrectSubtraction (Integer num1, Integer num2) {
+    public void shouldDoCorrectSubtraction (Integer num1, Integer num2, Integer expected) {
         String actual = serviceImpl.minus(num1, num2);
-        Integer result = num1 - num2;
-        Assertions.assertEquals (actual, num1 + " - " + num2 + " = " + result);
+        Assertions.assertEquals (actual, num1 + " - " + num2 + " = " + expected);
 
     }
     @ParameterizedTest
     @MethodSource("paramsForTestMultiplication")
-    public void shouldDoCorrectMultiplication (Integer num1, Integer num2) {
+    public void shouldDoCorrectMultiplication (Integer num1, Integer num2, Integer expected) {
         String actual = serviceImpl.multiply(num1, num2);
-        Integer result = num1 * num2;
-        Assertions.assertEquals (actual, num1 + " * " + num2 + " = " + result);
+        Assertions.assertEquals (actual, num1 + " * " + num2 + " = " + expected);
 
     }
 
     @ParameterizedTest
     @MethodSource("paramsForTestDivision")
-    public void shouldDoCorrectDivision (Integer num1, Integer num2) {
+    public void shouldDoCorrectDivision (Integer num1, Integer num2, Double expected) {
         if (num2 == 0) {
             DivideByZeroException e = Assertions.assertThrows (DivideByZeroException.class, () -> {
                 serviceImpl.divide(num1, 0);
@@ -48,37 +46,36 @@ public class ServiceImplParameterizedTest {
             Assertions.assertEquals("Делить на 0 нельзя", e.getMessage());
         } else {
             String actual = serviceImpl.divide(num1, num2);
-            Double result = (double) num1 / num2;
-            Assertions.assertEquals(actual, num1 + " / " + num2 + " = " + result);
+            Assertions.assertEquals(actual, num1 + " / " + num2 + " = " + expected);
         }
     }
     public static Stream<Arguments> paramsForTestAddiction() {
         return Stream.of (
-        Arguments.of (20, -10),
-        Arguments.of (-16, 4)
+        Arguments.of (20, -10, 10),
+        Arguments.of (-16, 4, -12)
         );
     }
 
     public static Stream<Arguments> paramsForTestSubtraction() {
         return Stream.of (
-                Arguments.of (22, -8),
-                Arguments.of (-16, 4)
+                Arguments.of (22, -8, 30),
+                Arguments.of (-16, 4, -20)
         );
     }
 
     public static Stream<Arguments> paramsForTestMultiplication() {
         return Stream.of (
-                Arguments.of (5, 5),
-                Arguments.of (-7, 3),
-                Arguments.of (-2, -3)
+                Arguments.of (5, 5, 25),
+                Arguments.of (-7, 3, -21),
+                Arguments.of (-2, -3, 6)
         );
     }
 
     public static Stream<Arguments> paramsForTestDivision() {
         return Stream.of (
-                Arguments.of (24, -4),
-                Arguments.of (-16, -8),
-                Arguments.of (1, 0)
+                Arguments.of (24, -4, -6.0),
+                Arguments.of (-16, -8, 2.0),
+                Arguments.of (1, 0, null)
         );
     }
 
